1cf37b9048484702597cafd145ccaa3b5f2d82e2
iris-starry <iris_starry_@naver.com>
2024-02-06 02:50:40 +0900

    :sparkles: 유저 조회 api 완성

--- endpoints/user.js
+++ endpoints/user.js
@@ -1,23 +1,8 @@
-// 회원가입 api
-
-// 유저 조회(이름, 아이디로,전체)
-// 유저 삭제
-
-
-
-// 좋아요 수 입력
-// 좋아요 수 보기
-// 좋아요 수 삭제
-
-// 좋아요 작품 추가
-// 좋아요 작품 보기
-// 좋아요 작품 삭제
 const express = require('express');
 const router = express.Router();
 const pool = require('../database/database');
 
 // 회원가입 api
-// 회원가입 엔드포인트
 router.post('/signup', async (req, res) => {
     try {
         const { name, user_id, password } = req.body;
@@ -44,6 +29,42 @@ router.post('/signup', async (req, res) => {
         res.status(500).json({ message: "Internal Server Error" });
     }
 });
+// 유저 조회 api
+router.get('/', async (req, res) => {
+    try {
+        // 데이터베이스에서 모든 유저 정보를 가져오는 쿼리
+        const query = 'SELECT * FROM User';
+
+        // 데이터베이스에서 유저 정보를 조회
+        pool.query(query, (error, results, fields) => {
+            if (error) {
+                console.error('Error retrieving users:', error);
+                return res.status(500).json({ message: "Internal Server Error" });
+            }
+            
+            res.status(200).json(results);
+        });
+    } catch (error) {
+        console.error('Error:', error);
+        res.status(500).json({ message: "Internal Server Error" });
+    }
+});
+
+// 유저 조회(이름, 아이디로,전체)
+// 유저 삭제
+
+
+
+// 좋아요 수 입력
+// 좋아요 수 보기
+// 좋아요 수 삭제
+
+// 좋아요 작품 추가
+// 좋아요 작품 보기
+// 좋아요 작품 삭제
+
+
+
 
 
 module.exports = router;
