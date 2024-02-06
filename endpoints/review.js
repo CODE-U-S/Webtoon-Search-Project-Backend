628cf44a5946819680e62f90aa3632d5c07a831b
darecoco <66424222+darecoco@users.noreply.github.com>
2024-02-07 00:24:07 +0900

    리뷰 달기 api 완성

--- endpoints/review.js
+++ endpoints/review.js
@@ -1,3 +1,44 @@
 const express = require('express');
 const router = express.Router();
+const { promisify } = require('util');
 const pool = require('../database/database');
+const queryAsync = promisify(pool.query).bind(pool);
+
+// 리뷰 달기 API
+router.post('/write', async (req, res) => {
+    try {
+        // 작품이름, 평점, 아이디, 댓글
+        const { title, rating, user_id, comment } = req.body;
+
+        // 유효성 검사
+        if (!title || !rating || !user_id || !comment) {
+            return res.status(400).json({ message: "작품이름, 평점, 아이디, 댓글을 전부 작성해 주세요." });
+        } else if (comment.length > 50) {
+            return res.status(400).json({ message: "댓글은 50글자 이내로 작성해 주세요." });
+        } else if (rating > 5 || rating < 0) {
+            return res.status(400).json({ message: "평점은 1점에서 5점까지만 가능합니다." });
+        }
+
+        // 유저 유효성 검사
+        const userValidationQuery = 'SELECT user_id FROM user WHERE user_id = ?';
+        const [userValidationResults, userValidationFields] = await queryAsync(userValidationQuery, [user_id]);
+        if (userValidationResults === undefined) {
+            // 유저가 없을 경우
+            return res.status(400).json({ message: "등록된 유저가 아닙니다." });
+        }
+
+        // DB 쿼리
+        const query = "INSERT INTO review (work_name, rating, user_id, comment) VALUES (?, ?, ?, ?)";
+
+        // DB 데이터 삽입
+        await queryAsync(query, [title, rating, user_id, comment]);
+
+        res.status(201).json({ message: "성공적으로 끝마쳤습니다!" });
+
+    } catch (error) {
+        console.error("에러 발생 : " + error);
+        res.status(500).json({ message: "에러가 발생하였습니다." });
+    }
+});
+
+module.exports = router;
