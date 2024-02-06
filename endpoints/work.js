7983a28112c6f67c09eadfceb167c3aadf85ef2a
iris-starry <iris_starry_@naver.com>
2024-02-07 01:33:10 +0900

    :zap: 작품 키워드 여러개 검색 업데이트

--- endpoints/work.js
+++ endpoints/work.js
@@ -6,20 +6,25 @@ const pool = require('../database/database');
 router.get('/', async (req, res) => {
     try {
         // 키워드를 추출합니다.
-        const keyword = req.query.keyword;
+        const keywords = req.query.keyword;
 
         // 유효성 검사
-        if (!keyword) {
-            return res.status(400).json({ message: "Keyword is required" });
+        if (!keywords || !Array.isArray(keywords) || keywords.length === 0) {
+            return res.status(400).json({ message: "Keywords are required and must be provided as an array" });
         }
 
         // 데이터베이스에서 키워드를 포함하는 작품 검색
-        const query = `SELECT * FROM work 
-                       WHERE title LIKE '%${keyword}%' 
-                          OR author LIKE '%${keyword}%' 
-                          OR genre LIKE '%${keyword}%' 
-                          OR day LIKE '%${keyword}%' 
-                          OR service LIKE '%${keyword}%'`;
+        let query = `SELECT * FROM work WHERE `;
+        for (let i = 0; i < keywords.length; i++) {
+            query += `(title LIKE '%${keywords[i]}%' 
+                      OR author LIKE '%${keywords[i]}%' 
+                      OR genre LIKE '%${keywords[i]}%' 
+                      OR day LIKE '%${keywords[i]}%' 
+                      OR service LIKE '%${keywords[i]}%')`;
+            if (i !== keywords.length - 1) {
+                query += ' AND ';
+            }
+        }
 
         // 작품 검색
         pool.query(query, (error, results, fields) => {
